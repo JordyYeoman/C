4d41d7bd1cf0f7a7511989546eae10b40256ef1a
Jordy Yeoman <jordy.yeoman@entaingroup.com.au>
2023-04-28 17:26:54 +0800

    chore: lecture-1 getting user input

---
+++ Lecture1/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char get_string(char *str) {
+ printf("Enter a string: ");
+ return scanf("%s", str); // Indicating that the following user input is a string via the %s identifier.
+}
+
+// Note 1 - %s is a format specifier, it means that the following argument is a string.
+int main(void) {
+    printf("Systems booting up...\n");
+
+    char bigBossString[100]; // Declaring a character array (char) to store the array of characters - aka 'string'
+    get_string(bigBossString); // Passing our string pointer to function that will collect user input.
+
+    printf("You entered: %s\n", bigBossString);
+}
